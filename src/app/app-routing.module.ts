b2d0ebe4d150e3fa0fc1ea429fda0fa20994eacf
Edycar Reyes <edycarreyes@gmail.com>
2021-01-31 18:20:42 -0600

    Created and implemented the NoAuthenticated guard files for the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {LoginViewComponent} from './views/login-view/login-view.component';
 import {PatientsViewComponent} from './views/patients-view/patients-view.component';
 import {AuthenticatedGuard} from './guards/authenticated.guard';
+import {NoAuthenticatedGuard} from './guards/no-authenticated.guard';
 
 const routes: Routes = [
   {
@@ -10,7 +11,8 @@ const routes: Routes = [
     component: LoginViewComponent,
     data: {
       slug: 'Inicio de sesión'
-    }
+    },
+    canActivate: [NoAuthenticatedGuard]
   },
   {
     path: '',
--- src/app/guards/no-authenticated.guard.ts
+++ src/app/guards/no-authenticated.guard.ts
@@ -6,7 +6,7 @@ import {AngularFireAuth} from '@angular/fire/auth';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticatedGuard implements CanActivate {
+export class NoAuthenticatedGuard implements CanActivate {
   constructor(
     private auth: AngularFireAuth,
     private router: Router
@@ -18,15 +18,15 @@ export class AuthenticatedGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return new Promise<boolean>(async (resolve, rejects) => {
       await this.auth.authState.subscribe((user) => {
-        if (user !== null) {
-          resolve(true);
+        if (user) {
+          this.router.navigateByUrl('').then((response) => console.log('navigated to login/'));
+          resolve(false);
         } else {
-          this.router.navigateByUrl('/login').then((response) => console.log('navigated to login/'));
-          rejects(false);
+          resolve(true);
         }
       }, error => {
-        console.error('Error implementing AuthenticationGuard:', error);
-        this.router.navigateByUrl('/login').then((response) => console.log('navigated to login/'));
+        console.error('Error implementing NoAuthenticated guard:', error);
+        this.router.navigateByUrl('').then((response) => console.log('navigated to login/'));
         rejects(false);
       });
     });
